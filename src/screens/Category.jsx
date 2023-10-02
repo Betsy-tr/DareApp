083c78e623041c27ed365020b966adb0820ad611
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-10-02 12:05:00 -0400

    maj

--- src/screens/Category.jsx
+++ src/screens/Category.jsx
@@ -45,7 +45,6 @@ const Category = ({navigation}) => {
         data={categories}
         renderItem={({item})=><ItemCategory category={item} />}
         keyExtractor={item=>item.id}
-        showDoneButton={false}
       />
       <Button mode='elevated' onPress={()=>navigation.navigate('TruthOrDare')}>NEXT</Button>
     
