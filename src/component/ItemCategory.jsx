7a9797bd2b6ee5e9153989deba8450a166b75b01
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-10-02 08:51:36 -0400

    maj

--- src/component/ItemCategory.jsx
+++ src/component/ItemCategory.jsx
@@ -1,18 +1,29 @@
 import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
+import { Text, View } from 'react-native';
 import { List } from 'react-native-paper';
+import styles from '../../styles';
 
 const ItemCategory = ({category}) => {
 
     const navigation = useNavigation()
 
-    return(
+   /* return(
 
         <List.Item
             title={category.name}
             onPress={()=>navigation.navigate('TruthOrDare' , {id: category.id})}
         />
-    )
+    )*/
+
+    return (
+        <View style={styles.slide}>
+          <Text style={styles.slideTitle}>{category.name}</Text>
+          {/* <Image source={{uri: category.image}} /> */}
+          <Text style={styles.slideText}>{category.description}</Text>
+          {/* <ImageBackground></ImageBackground> */}
+        </View>
+      );
 };
 
 export default ItemCategory;
