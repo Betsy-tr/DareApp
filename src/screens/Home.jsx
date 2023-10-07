62e429af913585b518d35e806fd215c12dd60295
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-10-07 11:18:07 -0400

    template

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text , ImageBackground} from 'react-native'
 import React from 'react'
 import styles from '../../styles'
 import { Button } from 'react-native-paper'
@@ -6,7 +6,13 @@ import { Button } from 'react-native-paper'
 const Home = ({navigation}) => {
   return (
     <View style={styles.container}>
+      <ImageBackground
+        resizeMode='cover'
+        style={{flex: 1}}
+        // source={require()}
+      >
       <Button mode='elevated' onPress={() => navigation.navigate('Players')}>Ajouter des joueurs</Button>
+      </ImageBackground>
     </View>
   )
 }
