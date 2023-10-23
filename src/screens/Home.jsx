b98d8e82241946c404e6f5d912345ea300add4b6
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-10-23 15:51:30 -0400

    maj

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -7,17 +7,10 @@ import { Button } from 'react-native-paper'
 const Home = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        // resizeMode='scretch'
-        style={{flex: 1}}
-        // width='100%'
-        source={require('../home.png')}
-      >
-      {/* <Button mode='elevated' onPress={() => navigation.navigate('Players')}>Ajouter des joueurs</Button> */}
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Players')}> 
-        <Image source={require('../buttonCommencer.png')} />
-      </TouchableOpacity>
-      </ImageBackground>
+      
+      <Button mode='elevated' onPress={() => navigation.navigate('Players')}>Ajouter des joueurs</Button>
+      
+      
     </View>
   )
 }
